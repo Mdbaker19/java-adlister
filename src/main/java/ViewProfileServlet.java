70f6253691b5c03051542e3631554f1f3097748d
Matthew <matthew.d.baker20@gmail.com>
2021-01-25 13:37:56 -0600

    finished sessions exercise added logout to partials navbar

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        boolean isLoggedIn = false;
+        String credential = null;
+
+        try{
+            isLoggedIn = (boolean) session.getAttribute("isLoggedIn");
+            credential = (String) session.getAttribute("user");
+        } catch (Exception ignored){}
+
+
+        if(credential == null){
+            System.out.println("souting for if user value has not been set");
+            response.sendRedirect("/login");
+            return;
+        }
+
+        if(!isLoggedIn){
+            System.out.println("you are not logged in and trying to visit the profile page, redirect to login page");
+            response.sendRedirect("/login");
+            return;
+        }
+
+
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
